265fc4fb6eeb22fc5dc7919f9306081c632c7d6e
B3IM4R <beimar.mestizo@gmail.com>
2024-05-28 23:06:29 -0500

    add support page validations

--- assets/js/utils/cleanForm.js
+++ assets/js/utils/cleanForm.js
@@ -1,3 +1,23 @@
+document.getElementById('formSupport').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+    const form = event.target;
+    
+    // Verifica la validez del formulario
+    if (form.checkValidity()) {
+        // Si el formulario es válido, muestra la modal manualmente
+        const modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+        modal.show();
+
+        // Limpia el formulario después de cerrar la modal
+        document.getElementById('staticBackdrop').addEventListener('hidden.bs.modal', function () {
+            limpiarFormulario();
+        }, { once: true }); // Elimina el eventListener después de ejecutarlo una vez
+    } else {
+        // Si el formulario no es válido, muestra mensajes de error
+        form.reportValidity();
+    }
+});
+
 function limpiarFormulario() {
     // Seleccionamos el formulario por su ID y lo reseteamos
     document.getElementById("formSupport").reset();
